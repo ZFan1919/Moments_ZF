095f7b8e71785be164c7f049bc329b7a69cb718a
ZFan1919 <145729443+ZFan1919@users.noreply.github.com>
2025-03-22 11:19:06 -0400

    Updated L1 Start event and added pick up & dropping sounds for L1

---
+++ Assets/Scripts/Interactions/L1Dropping.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity; // FMOD Studio for audio
+
+public class L1Dropping : MonoBehaviour
+{
+    public StudioEventEmitter DropSound; // Reference to the FMOD Event Emitter for the drop sound
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object has the "CanPickUp" tag
+        if (other.CompareTag("CanPickUp"))
+        {
+            // Play the drop sound
+            DropSound.Play();
+        }
+    }
+}
