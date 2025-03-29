95b373c0aa50111b5b7778d961adba1f93422a5e
ZFan1919 <145729443+ZFan1919@users.noreply.github.com>
2025-03-29 10:35:05 -0400

    Updated texts

--- Assets/Scripts/Interactions/L1ObjectDetector.cs
+++ Assets/Scripts/Interactions/L1ObjectDetector.cs
@@ -43,6 +43,6 @@ public class L1ObjectDetector : MonoBehaviour
 
     void UpdateCountText()
     {
-        countText.text = "Number of Office Objects Collected: " + collectedObjects.Count + " / " + requiredObjects.Count;
+        countText.text = "Number of Objects Collected: " + collectedObjects.Count + " / " + requiredObjects.Count;
     }
 }
